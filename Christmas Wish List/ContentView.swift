60d98e601e367cf937592083e658d77792f61a28
Caden Kowalski <cadenk@Cadens-MBP.fios-router.home>
2020-12-24 15:14:47 -0500

    Initialized app (1.0.0)

---
+++ Christmas Wish List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Christmas Wish List
+//
+//  Created by Caden Kowalski on 12/24/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
